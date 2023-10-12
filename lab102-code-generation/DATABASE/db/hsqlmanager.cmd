d568eeaa0151efa68e7a5ecd8fae1b9f3fc0fdc6
Jee-on <tjdgh3586@gmail.com>
2023-10-12 11:03:05 +0900

    lab 1-2 end

--- lab102-code-generation/DATABASE/db/hsqlmanager.cmd
+++ lab102-code-generation/DATABASE/db/hsqlmanager.cmd
@@ -1,1 +1,1 @@
-..\..\..\..\bin\jdk8u242-b08\bin\java -cp ./hsqldb-2.3.2.jar org.hsqldb.util.DatabaseManager -url jdbc:hsqldb:hsql://localhost/sampledb
+..\..\..\..\eGovFrame-4.1.0\bin\jdk8u242-b08\bin\java -cp ./hsqldb-2.3.2.jar org.hsqldb.util.DatabaseManager -url jdbc:hsqldb:hsql://localhost/sampledb
